d3df79b44ebb925d94b083f863cd54c5adead24f
Felipe <ffsumida@gmail.com>
2025-03-28 11:41:57 -0300

    Declaraçao e variaveis

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+// DECLARAÇAO E VARIAVEIS
+
+var nome ="Fiap";
+console.log(nome)
+
+let idade =19;
+console.log(idade)
+
+const sobrenome ="Fiapinho";
+console.log(sobrenome)
+
+// undefined
+
+let exemplo;
+console.log(exemplo)
+
+let exemplo1 =null;
+console.log(exemplo1)
+
+// TIPOS DE VARIAVEIS
+
+let 
